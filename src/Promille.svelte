fbd84a1f6bb643282a0d34d6b6a5413160c25ba8
Ola Gynnild Berg <s341874@oslomet.no>
2021-06-11 08:43:23 +0200

    Deleted commented code

--- src/Promille.svelte
+++ src/Promille.svelte
@@ -30,29 +30,3 @@
         justify-content: center;
     }
 </style>
-
-
-<!--<label>
-    Vekt:
-    <input type=number bind:value={vekt}>
-</label>
-<label>Timer siden drikkestart:
-    <input type=number bind:value={timer}>
-</label>
-<label>Forbrenning i timen:
-    <input type=number step=0.01 bind:value={forbrenning}>
-</label>
-<label>
-    <input type=radio bind:group={gender} value={0.7}>
-    Mann
-</label>
-<label>
-    <input type=radio bind:group={gender} value={0.6}>
-    Kvinne
-</label>
-<label>
-    <input type=radio bind:group={gender} value={0.65}>
-    Ikke-binær
-</label>
-<p>Alkohol: {gram.toFixed(2)} gram</p>
-<p>Promille: {promille > 0 ? promille.toFixed(2) : 0}</p> -->
