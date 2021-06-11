44cdc70831151db106769e7d74076be7fae2aa67
Ola Gynnild Berg <s341874@oslomet.no>
2021-06-11 09:16:37 +0200

    Row input og label på eksisterende drikke

--- src/DrinkWidget.svelte
+++ src/DrinkWidget.svelte
@@ -1,5 +1,6 @@
 <script>
     import {drinks} from "./stores";
+
     export let drink;
     export let index;
 
@@ -19,33 +20,44 @@
     }
 </script>
 
-    <div class="box">
+<div class="box">
     <div>
-        <h3>Drink</h3>
-        <p>Navn: {drink.name}</p>
-        <p>{drink.liter ? drink.liter : 0} liter</p>
-        <p>{drink.prosent ? drink.prosent : 0} prosent</p>
+        <h3>{drink.name}</h3>
     </div>
-    <label>
-        Liter:
-        <input type=number step=0.1 min=0 bind:value={drink.liter} on:input={updateArray}>
-    </label>
-    <label>
-        Prosent:
-        <input type=number step=0.1 min=0 max=100 bind:value={drink.prosent} on:input={updateArray}>
-    </label>
 
-        <button on:click={removeDrink}>Slett drink</button>
+    <div class="row">
+        <input id="liter" type=number step=0.1 min=0 max=100 bind:value={drink.liter} on:input={updateArray}>
+        <label for="liter">liter</label>
     </div>
+
+    <div class="row">
+        <input id="prosent" type=number step=0.1 min=0 max=100 bind:value={drink.prosent} on:input={updateArray}>
+        <label for="prosent">prosent</label>
+    </div>
+    <button on:click={removeDrink}>Slett drink</button>
+</div>
 
 <style>
     .box {
         box-sizing: border-box;
         width: min-content;
-        border: 2px solid rgba(0,0,0,0.05);
+        border: 2px solid rgba(0, 0, 0, 0.05);
         border-radius: 1em;
-        box-shadow: 5px 5px 5px rgba(0,0,0,0.2);
-        padding: 1em;
+        box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.2);
+        padding: 1em 4em 1em 4em;
         margin: 1em;
+    }
+
+    .row {
+        display: flex;
+        flex-direction: row;
+    }
+
+    input {
+        margin-right: 1em;
+    }
+
+    label {
+        padding-top: 0.3em;
     }
 </style>
