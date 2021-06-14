9b9b44f32970d317ea38a8dfac028039d02a8859
Ola Gynnild Berg <s341874@oslomet.no>
2021-06-14 12:46:17 +0200

    NaN check sober estimate

--- src/SoberEstimate.svelte
+++ src/SoberEstimate.svelte
@@ -9,9 +9,10 @@
         return estimate;
     }
     $: output = () => {
-        if (tid() === 0) return "";
-        if (tid() === 1) return "1 time til du er edru grovt estimert."
-        else return tid() + " timer til du er edru grovt estimert."
+        if (tid() === 0 || isNaN(tid())) return "";
+        else if (tid() === 1) return "1 time til du er edru grovt estimert.";
+        else if (tid() === Infinity) return "🤯 Uendelig timer til du er edru 🤯";
+        else return tid() + " timer til du er edru grovt estimert.";
     }
 </script>
 
